4d4c1066488903b57e723fe313d7b0d7d0b74c0b
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2018-09-11 00:07:33 +0200

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :get_layout
 
   # so that you get a normal error response
   rescue_from CanCan::AccessDenied do |exception|
@@ -6,5 +7,15 @@ class ApplicationController < ActionController::Base
     ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
     ## this render call should be:
     # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
+
+  private
+
+  def get_layout
+    if current_user.present?
+      'application_logged_in'
+    else
+      'application'
+    end
   end
 end
